9755fba47f21af6b23e8dc6c08afd7843e6d527a
kcypher13 <cypher@ftad.fr>
2015-03-19 14:42:23 +0100

    affiche array en liste

--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -1,12 +1,13 @@
 <?php
 
+
 	$article1 =array(
 		"title" => "Que diriez-vous d’explorer le centre de la Terre depuis votre navigateur ?",
 		"link" => "http://www.presse-citron.net/que-diriez-vous-dexplorer-le-centre-de-la-terre-depuis-votre-navigateur/",
 		"comments" => "http://www.presse-citron.net/que-diriez-vous-dexplorer-le-centre-de-la-terre-depuis-votre-navigateur/#comments",
 		"pubDate" => "Thu, 19 Mar 2015 08:41:20 +0000",
 		"guid" => "http://www.presse-citron.net/?p=129120",
-		"description" => "<![CDATA[La BBC vous invite à prendre votre casque et votre marteau piqueur, pour vous lancer dans une belle épopée virtuelle et stylisée.]]>",
+		"description" => "La BBC vous invite à prendre votre casque et votre marteau piqueur, pour vous lancer dans une belle épopée virtuelle et stylisée.",
 		"wfw:commentRss" =>"http://www.presse-citron.net/que-diriez-vous-dexplorer-le-centre-de-la-terre-depuis-votre-navigateur/feed/",
 	);
 
@@ -19,8 +20,16 @@
 	$article2['description'] = "<![CDATA[Internet Explorer, le navigateur qui fut roi pendant des années devrait disparaître à petit feu et être remplacé par un nouveau navigateur.]]>";
 	$article2['wdw:commentRss'] = "http://www.presse-citron.net/microsoft-annonce-la-mort-dinternet-explorer-son-celebre-navigateur/feed/";
 
-	var_dump($article2);
 
 
+	function convertArticleToList($article){
+		$list = '<ul>';
+		foreach ($article as $key => $value) {
+			$list .= '<li>'.$key.' : '.$value.'</li>';
+		}
+		return $list .= '</ul>';
+	};
+
+	echo (convertArticleToList($article2));
 
 ?>
